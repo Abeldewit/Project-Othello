076c7adf2e9e631a79648743b0fa26bf9304217a
Andreas <andreaszurhaar@gmail.com>
2018-09-18 12:08:19 +0200

    Added some abstract classes

---
+++ desktop/src/GameLogic/Pieces.java
@@ -0,0 +1,13 @@
+package GameLogic;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+
+public abstract class Pieces {
+
+    private Vector3 position;
+
+
+
+
+}
---
+++ desktop/src/GameLogic/Player.java
@@ -0,0 +1,13 @@
+package GameLogic;
+
+public abstract class Player {
+
+    private String playerColor;
+
+    //places the piece on the Board
+    public void place(){}
+    //Flips the piece to the apposing players color
+    public void flip(){}
+
+
+}
