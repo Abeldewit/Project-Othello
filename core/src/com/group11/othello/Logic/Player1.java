7514aef7f281d982a61bdf6802556720f31401b8
Andreas <andreaszurhaar@gmail.com>
2018-09-18 17:50:25 +0200

    Added Othello Board

---
+++ core/src/com/group11/othello/Logic/Player1.java
@@ -0,0 +1,15 @@
+package com.group11.othello.Logic;
+
+public class Player1 extends Player {
+    private String color;
+    private int score;
+
+    public Player1(String color)
+    {
+        super(color);
+    }
+
+
+
+
+}
---
+++ core/src/com/group11/othello/Logic/Player2.java
@@ -0,0 +1,12 @@
+package com.group11.othello.Logic;
+
+public class Player2 {
+    private String color;
+    private int score;
+
+    public Player2(String color)
+    {
+        this.color = color;
+        score = 0;
+    }
+}
