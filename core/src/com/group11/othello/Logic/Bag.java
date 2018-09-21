49c7a6b88208a7f8d169e06da19323334463aa28
Andreas <andreaszurhaar@gmail.com>
2018-09-21 16:44:22 +0200

    Started on GameLogic

--- core/src/com/group11/othello/Logic/Bag.java
+++ core/src/com/group11/othello/Logic/Bag.java
@@ -1,4 +1,15 @@
 package com.group11.othello.Logic;
 
 public class Bag {
+    private int chipCount;
+
+    public Bag(int chipCount)
+    {
+        this.chipCount = chipCount;
+    }
+
+    public int getChipCount()
+    {
+        return chipCount;
+    }
 }
--- core/src/com/group11/othello/Logic/GameLogic.java
+++ core/src/com/group11/othello/Logic/GameLogic.java
@@ -1,4 +1,39 @@
 package com.group11.othello.Logic;
 
 public class GameLogic {
+    private int playerCount;
+    private Player1 player1;
+    private Player2 player2;
+    private Bag bag;
+
+
+
+
+    public GameLogic(Player1 player1)
+    {
+        this.player1 = new Player1("white");
+        bag = new Bag(64);
+    }
+
+    public GameLogic(Player1 player1,Player2 player2)
+    {
+        this.player1 = new Player1("white");
+        this.player2 = new Player2("black");
+        bag = new Bag(64);
+    }
+
+
+    public boolean endGame()
+    {
+        boolean end = false;
+
+        if(bag.getChipCount() < 1 )
+        {
+            end = true;
+        }
+
+        return end;
+
+    }
+
 }
