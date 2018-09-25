0c8bf362ebd77bf9a01e0acf5a9a27af084dc17c
Abel de Wit <abelroef@hotmail.com>
2018-09-25 17:11:20 +0200

    Added click listener

---
+++ core/src/com/group11/othello/Game/clickProcessor.java
@@ -0,0 +1,43 @@
+package com.group11.othello.Game;
+
+
+import com.badlogic.gdx.InputProcessor;
+
+public class clickProcessor implements InputProcessor {
+    public boolean keyDown (int keycode) {
+        return false;
+    }
+
+    public boolean keyUp (int keycode) {
+        return false;
+    }
+
+    public boolean keyTyped (char character) {
+        return false;
+    }
+
+    public boolean touchDown (int x, int y, int pointer, int button) {
+
+        y -= 150;
+        x = (int) Math.floor(x/50);
+        y = (int) Math.floor(y/50);
+
+        return false;
+    }
+
+    public boolean touchUp (int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchDragged (int x, int y, int pointer) {
+        return false;
+    }
+
+    public boolean mouseMoved (int x, int y) {
+        return false;
+    }
+
+    public boolean scrolled (int amount) {
+        return false;
+    }
+}
