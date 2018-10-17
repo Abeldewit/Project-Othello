b93db218fccea12c4562bdabfcf8604cc8757e1c
Andreas <andreaszurhaar@gmail.com>
2018-10-17 12:45:38 +0200

    fixing some bugs

--- core/src/com/group11/othello/States/GameState.java
+++ core/src/com/group11/othello/States/GameState.java
@@ -66,10 +66,10 @@ public class GameState extends State {
                 int y = (int) Math.floor((Othello.HEIGHT - Gdx.input.getY()) / 100);
                 System.out.println("x = " + x);
                 System.out.println("y = " + y);
-                if(gL.endGame(gL.getTurnStatus()) == false)
+                if(gL.endGame(gL.getTurnStatus()) == true)
                 {
                     gL.changeTurn();
-                    if(gL.endGame(gL.getTurnStatus()) == false)
+                    if(gL.endGame(gL.getTurnStatus()) == true)
                     {
                         gsm.pop();
                     }
