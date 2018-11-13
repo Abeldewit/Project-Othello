b1c0b1580156094da04e9fa3c40acdfd7c2021cf
Abel de Wit <abelroef@hotmail.com>
2018-11-13 16:37:54 +0100

    Start of NegaMax

--- core/src/com/group11/othello/AI/NegaMax/NegaMax.java
+++ core/src/com/group11/othello/AI/NegaMax/NegaMax.java
@@ -1,10 +1,38 @@
-package com.group11.othello.AI.Greedy;
+package com.group11.othello.AI.NegaMax;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.group11.othello.Logic.GameLogic;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class NegaMax {
 
+    GameLogic NegaGl;
 
 
+    public NegaMax(GameLogic gameLogic) {
+        NegaGl = gameLogic.copy();
+    }
 
+    public Iterable<Vector2> getBestMoves(int depth) {
+        NegaGl.getValidMoves();
+        if (depth <= 0) {
+            throw new IllegalArgumentException("Search depth MUST be > 0");
+        }
+        Iterable<Vector2> iterator = NegaGl.getValidMoves();
+        return iterator;
+    }
+
+    public void negaMax(int depth) {
+        if(depth == 0) {
+            //return evaluate();
+        }
+
+
+        
+    }
 
 
 }
