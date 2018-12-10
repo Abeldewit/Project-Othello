b17fabbc343860a83bee688517e10fbf8a3deded
sandu <sandu.zuza@mail.ru>
2018-12-10 15:04:55 +0100

    Game before crash with working testing

--- core/src/com/group11/othello/AI/Testing.java
+++ core/src/com/group11/othello/AI/Testing.java
@@ -20,7 +20,7 @@ public class Testing {
 
     public void addRecords(String firstAiName,String secondAiName,String ev1,String ev2,String ev3,float score,String winner) throws IOException {
         PrintWriter bw = new PrintWriter(new FileOutputStream("Tests.txt",true));
-        String row = firstAiName + " | " + secondAiName + " | " + ev1 + " | " + ev2 + " | " + ev3 + " | " + score + " | " + newline;
+        String row = firstAiName + " | " + secondAiName + " | " + ev1 + " | " + ev2 + " | " + ev3 + " | " + score + " | " +  " | " + winner + newline;
         bw.write(row);
         bw.close();
         System.out.println("Wrote");
